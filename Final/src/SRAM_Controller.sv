f26b4a24dda3695356de911399748012da509dbf
TAN9008 <sioefjfk@gmail.com>
2025-06-09 20:04:07 +0800

    Add RECOGNITION state for core

--- Final/src/SRAM_Controller.sv
+++ Final/src/SRAM_Controller.sv
@@ -1,10 +1,12 @@
 //******* VER_1 *********//
 //*******  0609  ********//
-//**Todo: add mem_valid**// 
+//*******  1959  ********//
+
 module SRAM_Controller(
 
 i_clk,
 i_rst,
+inputtimming, 
 // Input assignment
 core_mem_w_value, 
 core_mem_addr,
@@ -25,8 +27,9 @@ o_SRAM_UB_N,
 io_SRAM_DQ
 );
 
-input  i_clk;
-input  i_rst;
+input i_clk;
+input i_rst;
+input inputtimming; 
 // Pins to Top Module 
 input  core_mem_w_value [15:0];
 input  core_mem_addr    [19:0];
@@ -48,6 +51,7 @@ output o_SRAM_UB_N;
 inout io_SRAM_DQ		[15:0];
 
 logic [3:0]  counter_r, counter_w;
+logic [8:0]  addr_count_r, addr_count_w;
 logic [2:0]  state_r, state_w;
 logic [15:0] IN_DATA_w, IN_DATA_r;
 logic [19:0] IN_ADDR_w, IN_ADDR_r;
@@ -57,16 +61,19 @@ localparam IDLE        = 3'b000,
 		   READ        = 3'b001,
 		   READ_BACK   = 3'b011,
 		   WRITE       = 3'b010,
-		   WRITE_BACK  = 3'b110;
+		   WRITE_BACK  = 3'b110, 
+		   RECOG_CTRL  = 3'b100;
 		   
 localparam LIMIT = 4'b0111;
+localparam LetterNum = 9'd416;
 
 assign o_SRAM_CE_N = 1'b0 ;
 assign o_SRAM_OE_N = 1'b0 ;
 assign o_SRAM_LB_N = 1'b0 ;
 assign o_SRAM_UB_N = 1'b0 ;
 
-assign mem_valid = ((state_r == READ_BACK) || (state_r == WRITE_BACK));
+assign mem_valid = ((state_r == READ_BACK) || (state_r == WRITE_BACK) || ((state_r == RECOG_CTRL) && (counter_r == LIMIT + 1)));
+assign addr_count_w = ((state_r == RECOG_CTRL) && (addr_count_r < LetterNum) && (counter_r == LIMIT + 1)) ? addr_count_r + 1 : 0;
 
 always@(*)begin: FSM
 	state_w = state_r;
@@ -74,6 +81,9 @@ always@(*)begin: FSM
 		IDLE: begin
 			if(core_mem_request)begin
 				state_w = (core_mem_wr) ? READ : WRITE;
+			end
+			else if(inputtimming)begin
+				state_w = RECOG_CTRL;
 			end
 		end
 		READ: begin
@@ -88,11 +98,20 @@ always@(*)begin: FSM
 		WRITE_BACK: begin
 			state_w = IDLE;
 		end
+		RECOG_CTRL: begin
+			state_w = (addr_count_r == LetterNum) ? IDLE : RECOG_CTRL;
+		end
 	endcase
 end
 
 always@(*)begin: Counter_Assignment
-	counter_w = (state_r) ? ((counter_r == LIMIT) ? 0 : counter_r + 1) : 0;
+	counter_w = counter_r;
+	if(state_r == PROCESS)begin
+		counter_w = (counter_r == LIMIT + 1) ? 0 : counter_r + 1;
+	end
+	else begin
+		counter_w = (state_r) ? ((counter_r == LIMIT) ? 0 : counter_r + 1) : 0;
+	end
 end
 
 always@(*)begin: Input_Capture
@@ -100,28 +119,30 @@ always@(*)begin: Input_Capture
 	IN_ADDR_w = (core_mem_request) ? core_mem_addr    : IN_ADDR_r;
 end
 
-assign o_SRAM_ADDR  = (core_mem_request) ? core_mem_addr : IN_ADDR_r;
+assign o_SRAM_ADDR  = (core_mem_request) ? core_mem_addr : (state_r == RECOG_CTRL) ? {11'b0, addr_count_r} : IN_ADDR_r;
 assign io_SRAM_DQ   = (state_r == WRITE) ? IN_DATA_r     :     16'dz;
 assign o_SRAM_WE_N  = (state_r == WRITE) ? 1'b0          :     	1'b1;
 
 assign core_mem_r_value = DATA_BACK_r;
-assign DATA_BACK_w      = (state_r == READ)  ? io_SRAM_DQ: DATA_BACK_r;   
-assign core_wait        = ((state_r == READ) || (state_r == WRITE));
+assign DATA_BACK_w      = ((state_r == READ) || (state_r == RECOG_CTRL))  ? io_SRAM_DQ: DATA_BACK_r;   
+assign core_wait        = ((state_r == READ) || (state_r == WRITE) || ((state_r == RECOG_CTRL) && (counter_r <= LIMIT)));
 
 always@(posedge i_clk or negedge i_rst)begin: Flip_Flop
 	if(!i_rst)begin
-		state_r   	<= IDLE;
-		counter_r 	<= 0;
-		IN_DATA_r 	<= 0;
-		IN_ADDR_r   <= 0;
-		DATA_BACK_r <= 0;
+		state_r   	 <= IDLE;
+		counter_r 	 <= 0;
+		IN_DATA_r 	 <= 0;
+		IN_ADDR_r    <= 0;
+		DATA_BACK_r  <= 0;
+		addr_count_r <= 0;
 	end
 	else begin
-		state_r   	<= state_w;
-		counter_r 	<= counter_w;
-		IN_DATA_r 	<= IN_DATA_w;
-		IN_ADDR_r 	<= IN_ADDR_w;
-		DATA_BACK_r <= DATA_BACK_w;
+		state_r   	 <= state_w;
+		counter_r 	 <= counter_w;
+		IN_DATA_r 	 <= IN_DATA_w;
+		IN_ADDR_r 	 <= IN_ADDR_w;
+		DATA_BACK_r  <= DATA_BACK_w;
+		addr_count_r <= addr_count_w;
 	end
 end
   
