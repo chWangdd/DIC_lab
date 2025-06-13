2225b58eb9169b7e62b85b22c8ed03a2157be755
chinthomas <chengyou928@gmail.com>
2025-06-13 13:02:17 +0800

    trajAdder can store multi-points

--- Final/src/trajectoryAdder.sv
+++ Final/src/trajectoryAdder.sv
@@ -11,22 +11,25 @@ module TrajAdder (
   output [29:0] o_color
 );
   
-  localparam lineWidth = 1;
-  `define frameWidth = 640;
-  `define frameHeight = 480;
+  localparam lineWidth = 3;
+  `define frameWidth 640
+  `define frameHeight 480
   // =========================================
   // Reg/wire declarations
   // =========================================
   reg [29:0] trajColor;
   reg [ 9:0] pointH;
   reg [ 9:0] pointV;
-  // reg [`frameWidth-1: 0] trag[0: `frameHeight-1];
+  reg [ 6:0] trag [0: `frameWidth-1][0: `frameHeight-1];
   wire isTrajectory;
+
+  integer i, j;
   // =========================================
   // wire assignments
   // =========================================
-  assign isTrajectory = (i_h >= pointH + lineWidth && i_h <= pointH + lineWidth && 
-                         i_v >= pointV + lineWidth && i_v <= pointV + lineWidth);
+  // assign isTrajectory = (i_h >= pointH + lineWidth && i_h <= pointH + lineWidth && 
+  //                        i_v >= pointV + lineWidth && i_v <= pointV + lineWidth);
+  assign isTrajectory = trag[i_h][i_v][6];
   assign o_color = (!i_rendering) ? i_color:
                    (!isTrajectory)? i_color: trajColor;
   
@@ -35,6 +38,11 @@ module TrajAdder (
       trajColor <= {10'd0, 8'd200, 12'd0};
       pointH <= {10{1'd1}};
       pointV <= {10{1'd1}};
+      for (i = 0; i < `frameWidth; i = i+1) begin
+        for (j = 0; j < `frameHeight; j = j+1) begin
+          trag[i][j] <= 0;
+        end
+      end
     end
     else begin
       trajColor <= trajColor;
@@ -43,6 +51,18 @@ module TrajAdder (
       if (i_pointVAL) begin
         pointH <= i_pointH;
         pointV <= i_pointV;
+      end
+      for (i = 0; i < `frameWidth; i = i+1) begin
+        for (j = 0; j <`frameHeight; j = j+1) begin
+          if (i_pointVAL && 
+          (i <= i_pointH + lineWidth && i >= i_pointH) && 
+          (j <= i_pointV + lineWidth && j >= i_pointV))
+            trag[i][j] <= {1'b1, {6{1'b0}}};
+          else if (trag[i][j] != 0)
+            trag[i][j] <= trag[i][j] + 1;
+          else
+            trag[i][j] <= trag[i][j];
+        end
       end
     end
   end
